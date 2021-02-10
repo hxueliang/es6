e7fe8031fe2b39ea5ce27a356a849cd66caec3bc
huangxueliang <huangxueliang@lbdfun.com>
2021-02-10 20:42:30 +0800

    feat: 继承

--- class/base-dome/index.js
+++ class/base-dome/index.js
@@ -7,7 +7,32 @@ class MathHandle {
     return x + y
   }
 }
-
 const m = new MathHandle(1, 2)
-
 console.log(m.add(1, 2))
+
+console.log('typeof:', typeof MathHandle)
+console.log('是否相同:', MathHandle === MathHandle.prototype.constructor)
+console.log('是否相同:', m.__proto__ === MathHandle.prototype)
+
+
+// 继承
+class Animal {
+  constructor(name) {
+    this.name = name
+  }
+  eat() {
+    console.log(`${this.name} eat`)
+  }
+}
+class Dog extends Animal {
+  constructor(name, age) {
+    super(name)
+    this.age = age
+  }
+  say() {
+    console.log(`${this.name} ${this.age}岁了`)
+  }
+}
+const d1 = new Dog('小黄', 8)
+d1.eat()
+d1.say()
